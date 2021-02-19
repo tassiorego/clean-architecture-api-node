98b83dfda444bf7cbf088e05d53ef3dcf36407c0
Tassio Rego <tassiorego@gmail.com>
2021-02-19 17:12:18 -0300

    feat: ensure CreateAccountController return missing param error if no name is provided

--- src/presentation/controllers/CreateAccountController.ts
+++ src/presentation/controllers/CreateAccountController.ts
@@ -2,6 +2,7 @@ export default class CreateAccountController {
   public handle(httpRequest: any): any {
     return {
       statusCode: 400,
+      body: new Error('Missing param: name'),
     };
   }
 }
