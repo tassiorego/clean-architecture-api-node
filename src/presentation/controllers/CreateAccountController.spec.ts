66555297f8965bb1e0945398e37b89b3d1fae6fc
Tassio Rego <tassiorego@gmail.com>
2021-02-19 16:35:57 -0300

    test: ensure CreateAccountController return 400 if no name is provided

---
+++ src/presentation/controllers/CreateAccountController.spec.ts
@@ -0,0 +1,18 @@
+import { CreateAccountController } from './CreateAccountController';
+
+describe('CreateAccountController', () => {
+  it('should return 400 if no name is provided', () => {
+    const sut = new CreateAccountController();
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmatiom: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
