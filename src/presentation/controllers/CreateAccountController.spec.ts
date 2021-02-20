18ed82aa244cc91e86e15ce69c103dd2573e5963
Tassio Rego <tassiorego@gmail.com>
2021-02-20 08:55:57 -0300

    test: ensure CreateAccount return 400 if no email is provided

--- src/presentation/controllers/CreateAccountController.spec.ts
+++ src/presentation/controllers/CreateAccountController.spec.ts
@@ -17,4 +17,20 @@ describe('CreateAccountController', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('name'));
   });
+
+  it('should return 400 if no email is provided', () => {
+    const sut = new CreateAccountController();
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmatiom: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('email'));
+  });
 });
