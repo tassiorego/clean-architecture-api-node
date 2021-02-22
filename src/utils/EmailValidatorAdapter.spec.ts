c27f360766a6aaec669ded62c24d091d4912c3fa
Tassio Rego <tassiorego@gmail.com>
2021-02-22 10:25:33 -0300

    refactor: add instance to factory helper

--- src/utils/EmailValidatorAdapter.spec.ts
+++ src/utils/EmailValidatorAdapter.spec.ts
@@ -1,22 +1,27 @@
 import validator from 'validator';
+import { EmailValidator } from '../presentation/protocols';
 import EmailValidatorAdapter from './EmailValidatorAdapter';
+
+const makeSut = (): EmailValidator => {
+  return new EmailValidatorAdapter();
+};
 
 describe('EmailValidator Adapter', () => {
   test('should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
     const isValid = sut.isValid('invalid_email@mail.com');
     expect(isValid).toBe(false);
   });
 
   test('should return true if validator returns false', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isValid = sut.isValid('valid_email@mail.com');
     expect(isValid).toBe(true);
   });
 
   test('should call validator with correct email', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isEmailSpy = jest.spyOn(validator, 'isEmail');
     sut.isValid('valid_email@mail.com');
     expect(isEmailSpy).toHaveBeenCalledWith('valid_email@mail.com');
