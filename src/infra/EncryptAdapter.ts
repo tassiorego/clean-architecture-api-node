bed8865d9b8cfcdf389fb194dd8c5bbac4878590
Tassio Rego <tassiorego@gmail.com>
2021-02-22 17:03:39 -0300

    feat: ensure EncriptAdapter call bcrypt with correct values

---
+++ src/infra/EncryptAdapter.ts
@@ -0,0 +1,14 @@
+import * as bcrypt from 'bcrypt';
+import { Encrypter } from '../data/protocols/Encrypter';
+
+export default class EncryptAdapter implements Encrypter {
+  private readonly salt: number;
+
+  constructor(salt) {
+    this.salt = salt;
+  }
+
+  public async encrypt(value: string): Promise<string> {
+    return bcrypt.hash(value, 12);
+  }
+}
