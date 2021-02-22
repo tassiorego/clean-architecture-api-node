2d9aac63c83ee929448c661e408ac7820e237d3e
Tassio Rego <tassiorego@gmail.com>
2021-02-22 12:06:58 -0300

    refactor: move code to a factory helper

--- src/data/usecases/account/CreateAccountService.spec.ts
+++ src/data/usecases/account/CreateAccountService.spec.ts
@@ -1,24 +1,34 @@
 import { Encrypter } from '../../protocols/Encrypter';
 import CreateAccountService from './CreateAccountService';
 
+const makeSut = (): any => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(): Promise<string> {
+      return new Promise(resolve => resolve('hashed_password'));
+    }
+  }
+
+  const encryptStub = new EncrypterStub();
+
+  const sut = new CreateAccountService(encryptStub);
+
+  return {
+    sut,
+    encryptStub,
+  };
+};
+
 describe('DBCreateAccount Usecase', () => {
   test('should call Encrypter with correct password', async () => {
-    class EncrypterStub implements Encrypter {
-      async encrypt(): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'));
-      }
-    }
-
     const accountData = {
       name: 'valid_name',
       email: 'valid_email',
       password: 'valid_password',
     };
-    const encryptStub = new EncrypterStub();
+
+    const { sut, encryptStub } = makeSut();
 
     const encryptSpy = jest.spyOn(encryptStub, 'encrypt');
-
-    const sut = new CreateAccountService(encryptStub);
 
     await sut.execute(accountData);
 
