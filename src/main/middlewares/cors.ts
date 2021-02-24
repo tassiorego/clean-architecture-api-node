08e2d2ee6a27469e5485241207234df0a62ba6fa
Tassio Rego <tassiorego@gmail.com>
2021-02-24 17:15:53 -0300

    test: ensure cors is enabled

---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from 'express';
+
+const cors = (req: Request, res: Response, next: NextFunction): void => {
+  res.set('access-control-allow-origin', '*');
+  res.set('access-control-allow-headers', '*');
+  res.set('access-control-allow-methods', '*');
+  next();
+};
+
+export default cors;
