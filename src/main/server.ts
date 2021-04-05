4ce67c28d23503da2132ab4f07343b27d3b25a7a
Tassio Rego <tassiorego@gmail.com>
2021-04-04 21:44:57 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,12 @@
-import app from './config/app';
 import 'dotenv/config';
+import MongoHelper from '../infra/database/mongodb/helpers/MongoHelper';
+import env from './config/env';
 
-app.listen(process.env.API_PORT);
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default;
+    app.listen(env.port, () =>
+      console.log(`🚀 Server running at localhost:${env.port}`),
+    );
+  })
+  .catch(console.error);
