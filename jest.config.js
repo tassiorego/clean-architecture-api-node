f9810bc04c059654eced5e9d14554dd4c213522c
Tássio Rego <tassiorego@gmail.com>
2021-06-05 22:33:00 -0300

    chore: add new rule

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,6 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   preset: '@shelf/jest-mongodb',
-  // coverageProvider: 'v8',
-
-
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
   transform: {
