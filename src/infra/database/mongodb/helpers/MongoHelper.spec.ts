d7eac3779e1c63934982f3c4f38242c87b096eff
Tássio Rego <tassiorego@gmail.com>
2021-09-20 20:23:05 -0300

    test: ensure mongodb reconnect if client is down

---
+++ src/infra/database/mongodb/helpers/MongoHelper.spec.ts
@@ -0,0 +1,18 @@
+import MongoHelper from './MongoHelper';
+
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('should reconnect if mongodb is down', async () => {
+    let accountCollection = await MongoHelper.getCollection('accounts');
+    expect(accountCollection).toBeTruthy();
+    await MongoHelper.disconnect();
+    accountCollection = await MongoHelper.getCollection('accounts');
+    expect(accountCollection).toBeTruthy();
+  });
+});
